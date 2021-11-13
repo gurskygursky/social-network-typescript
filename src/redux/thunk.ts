71fa0c7b151e0617080474b1f0adf36143016c6e
gurskygursky <yahorhurski@gmail.com>
2021-11-13 22:34:25 +0300

    refactoring thunk

--- src/redux/thunk.ts
+++ src/redux/thunk.ts
@@ -10,9 +10,10 @@ import {
     SetUsersTotalCount,
     ToggleFollowingInProgress,
     ToggleIsFetching,
-    UnfollowUser, LoginUserForm, ActionsTypes,
+    UnfollowUser,
+    ActionsTypes,
 } from "./actions";
-import {ThunkAction, ThunkActionDispatch, ThunkDispatch} from "redux-thunk";
+import {ThunkAction} from "redux-thunk";
 import {RootStateType} from "./redux-store";
 
 export const getUsersThunkCreator = (currentPage: number, pageSize: number) => (dispatch: Dispatch) => {
@@ -33,79 +34,56 @@ export const selectPageThunkCreator = (numberPage: number, pageSize: number) => 
             dispatch(SetUsers(data.items));
         });
 }
-export const followUserThunk = (userID: number) => (dispatch: Dispatch) => {
+export const followUserThunk = (userID: number) => async (dispatch: Dispatch) => {
     dispatch(ToggleFollowingInProgress(true, userID));
-    UsersAPI.follow(userID)
-        .then(responce => {
-            if (responce.data.resultCode === 0) {
-                dispatch(FollowUser(userID));
-            }
-            dispatch(ToggleFollowingInProgress(false, userID));
-        })
+    const response = await UsersAPI.follow(userID)
+    if (response.data.resultCode === 0) {
+        dispatch(FollowUser(userID));
+    }
+    dispatch(ToggleFollowingInProgress(false, userID));
 }
-export const unfollowUserThunk = (userID: number) => (dispatch: Dispatch) => {
+export const unfollowUserThunk = (userID: number) => async (dispatch: Dispatch) => {
     dispatch(ToggleFollowingInProgress(true, userID));
-    UsersAPI.unfollow(userID)
-        .then(responce => {
-            if (responce.data.resultCode === 0) {
-                dispatch(UnfollowUser(userID));
-            }
-            dispatch(ToggleFollowingInProgress(false, userID));
-        });
+    const response = await UsersAPI.unfollow(userID);
+    if (response.data.resultCode === 0) {
+        dispatch(UnfollowUser(userID));
+    }
+    dispatch(ToggleFollowingInProgress(false, userID));
 }
 
-export const loginUserThunk = () => (dispatch: Dispatch) => {
-    HeaderAPI.authMe()
-        .then(responce => {
-            console.log(responce)
-            if (responce.data.resultCode === 0) {
-                let {id, email, login} = responce.data.data
-                dispatch(LoginUser(id, email, login, true))
-                // console.log(responce.data.data)
-                // dispatch(LoginUser(responce.data.data,  ));
-            }
-        });
+export const loginUserThunk = () => async (dispatch: Dispatch) => {
+    const response = await HeaderAPI.authMe()
+    if (response.data.resultCode === 0) {
+        let {id, email, login} = response.data.data
+        dispatch(LoginUser(id, email, login, true))
+    }
 }
 
-export const selectUserProfileThunk = (userID: number) => (dispatch: Dispatch) => {
-    ProfileAPI.selectUserProfile(userID)
-        .then(responce => {
-            dispatch(SelectUserProfile(responce.data));
-        })
+export const selectUserProfileThunk = (userID: number) => async (dispatch: Dispatch) => {
+    const response = await ProfileAPI.selectUserProfile(userID);
+    dispatch(SelectUserProfile(response.data));
 }
 
-export const getUserStatusThunk = (userID: number) => (dispatch: Dispatch) => {
-    ProfileAPI.getStatus(userID)
-        .then(response => {
-            console.log(response)
-            dispatch(SetUserStatus(response.data))
-        })
+export const getUserStatusThunk = (userID: number) => async (dispatch: Dispatch) => {
+    const response = await ProfileAPI.getStatus(userID);
+    dispatch(SetUserStatus(response.data))
 }
-export const changeUserStatusThunk = (statusText: string) => (dispatch: Dispatch) => {
-    ProfileAPI.changeStatus(statusText)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(SetUserStatus(statusText))
-            }
-        })
+export const changeUserStatusThunk = (statusText: string) => async (dispatch: Dispatch) => {
+    const response = await ProfileAPI.changeStatus(statusText);
+    if (response.data.resultCode === 0) {
+        dispatch(SetUserStatus(statusText))
+    }
 }
-export const loginThunk = (email: string, password: string, rememberMe: boolean): ThunkAction<void, RootStateType, unknown , ActionsTypes> => (dispatch) => {
-    HeaderAPI.login(email, password, rememberMe)
-        .then(response => {
-            console.log(response)
-            if (response.data.resultCode === 0) {
-                dispatch(loginUserThunk())
-                // dispatch(LoginUser(response.data.data, true))
-            }
-        })
+export const loginThunk = (email: string, password: string, rememberMe: boolean): ThunkAction<void, RootStateType, unknown, ActionsTypes> => async (dispatch) => {
+    const response = await HeaderAPI.login(email, password, rememberMe)
+    if (response.data.resultCode === 0) {
+        dispatch(loginUserThunk());
+    }
 }
-export const logoutThunk = () => (dispatch: Dispatch) => {
-    debugger
-    HeaderAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(LoginUser(null, '', '', false))
-            }
-        })
+export const logoutThunk = () => async (dispatch: Dispatch) => {
+    const response = await HeaderAPI.logout()
+    if (response.data.resultCode === 0) {
+        dispatch(LoginUser(null, '', '', false))
+    }
 }
 
